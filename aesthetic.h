4969665fbfce5f525935e1e72a0da722e6a7065b
Daniel V Mathew <danielvmathew27@gmail.com>
2023-05-06 19:00:37 +0530

    Ok this is the initial commit after patching st
    Because I didn't know how to use git back then... :)

---
+++ aesthetic.h
@@ -0,0 +1,28 @@
+static const char *colorname[] = { //aesthetic night 
+	/* 8 normal colors */ 
+	"#1C252C", 
+	"#DF5B61", 
+	"#78B892", 
+	"#DE8F78", 
+	"#6791C9", 
+	"#BC83E3", 
+	"#67AFC1", 
+	"#D9D7D6", 
+
+	/* 8 bright colors */ 
+
+	"#484E5B", 
+	"#F16269", 
+	"#8CD7AA", 
+	"#E9967E", 
+	"#79AAEB", 
+	"#C488EC", 
+	"#7ACFE4", 
+	"#E5E5E5", 
+
+	[256] = "#E5E5E5", /* default foreground colour */ 
+	[257] = "#06111d", /* default background colour */ 
+	[258] = "#D9D7D6", /*575268*/ 
+//06111d
+
+}; 
---
+++ catppuccin.h
@@ -0,0 +1,27 @@
+static const char *colorname[] = { //catppuccin 
+	/* 8 normal colors */ 
+	"#6E6C7E", 
+	"#F28FAD", 
+	"#ABE9B3", 
+	"#FAE3B0", 
+	"#96CDFB", 
+	"#F5C2E7", 
+	"#89DCEB", 
+	"#D9E0EE", 
+
+	/* 8 bright colors */ 
+	"#988BA2", 
+	"#F28FAD", 
+	"#ABE9B3", 
+	"#FAE3B0", 
+	"#96CDFB", 
+	"#F5C2E7", 
+	"#89DCEB", 
+	"#D9E0EE", 
+
+
+	[256] = "#D9E0EE", /* default foreground colour */ 
+	[257] = "#1E1E2E", /* default background colour */ 
+	[258] = "#C3BAC6", /*575268*/ 
+
+}; 
